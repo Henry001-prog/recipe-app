9ff22876c823b69d4ca90d384b6986cb230a0f56
Henry001-prog <alissonhenrique001@hotmail.com.br>
2024-07-11 23:40:37 -0300

    initial structure

---
+++ src/LoginScreen/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title, Text } from "./styles";
+
+export function LoginScreen() {
+    return (
+        <Container>
+            <Title>Encontre Seu Próximo Prato Favorito</Title>
+        </Container>
+    );
+}
---
+++ src/LoginScreen/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #20412e;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    font-size: 17px;
+    color: #f9fffb;
+`;
+
+export const Text = styled.Text`
+    font-size: 13px;
+    color: #f9fffb;
+`;
