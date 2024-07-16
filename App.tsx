368403b1adcbf69d28afae5a3486f1f469782b1a
Henry001-prog <alissonhenrique001@hotmail.com.br>
2024-07-16 00:57:55 -0300

    clean code

--- App.tsx
+++ App.tsx
@@ -38,19 +38,5 @@ export default function App() {
         <Routes />
       </ThemeProvider>
     </ThemeContext.Provider>
-
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#20412e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
